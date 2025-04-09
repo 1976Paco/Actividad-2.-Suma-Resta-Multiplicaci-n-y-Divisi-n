def86711b96cb4d5d9baeebc35be7b14369051e5
Francisco Zapien Bueno <163362949+1976Paco@users.noreply.github.com>
2025-04-09 07:25:28 -0600

    Add files via upload

---
+++ luis2025_ac3/src/luis2025_ac3/Luis2025_ac3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package luis2025_ac3;
+
+/**
+ *
+ * @author LUIS
+ */
+public class Luis2025_ac3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
